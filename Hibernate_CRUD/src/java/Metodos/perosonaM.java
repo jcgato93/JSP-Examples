cae5a1cc4a567b9678bd805c806f1b75fa1a51ac
jcgato93 <npgcamilo@gmail.com>
2017-11-18 16:29:14 -0500

    Hibernate CRUD

--- Hibernate_CRUD/src/java/Metodos/perosonaM.java
+++ Hibernate_CRUD/src/java/Metodos/perosonaM.java
@@ -13,7 +13,11 @@ import org.hibernate.Transaction;
 
 
 public class  perosonaM {
-    
+   
+    /**
+     * Retorna la lista de todas las personas
+     * @return 
+     */       
     public ArrayList<Persona> getPersona()
     {
         SessionFactory sesion=HibernateUtil.getSessionFactory();
@@ -39,4 +43,85 @@ public class  perosonaM {
         
         return persona;
     }
+    
+    /**
+     * Retorna una persona segun el Id
+     * @param id
+     * @return 
+     */
+    public Persona getPersonaPorId(int id)
+    {
+     SessionFactory factory=HibernateUtil.getSessionFactory();
+     Session con=factory.openSession();
+     Transaction tx=con.beginTransaction();
+     
+     Persona persona=(Persona)con.get(Persona.class,id);
+     
+     tx.commit();
+     con.close();
+     
+     return persona;
+    }
+    
+    
+    /**
+     * Actualiza una persona por id
+     * @param id 
+     * @param p 
+     */
+    public void updatePersona(int id,Persona p)
+    {
+     SessionFactory factory=HibernateUtil.getSessionFactory();
+     Session con=factory.openSession();
+     Transaction tx=con.beginTransaction();
+     
+     Persona persona=(Persona)con.get(Persona.class,id);
+     
+     persona.setNombres(p.getNombres());
+     persona.setApellidos(p.getApellidos());
+     
+     con.update(persona);
+     
+     tx.commit();
+     con.close();
+     
+     
+    }
+    
+    
+    /**
+     * Borra una persona por Id
+     * @param id 
+     */
+    public void deletePersona(int id)
+    {
+        SessionFactory factory=HibernateUtil.getSessionFactory();
+     Session con=factory.openSession();
+     Transaction tx=con.beginTransaction();
+     
+     Persona persona=(Persona)con.get(Persona.class,id);
+     
+     con.delete(persona);
+     
+     tx.commit();
+     con.close();
+    }
+    
+    
+    /**
+     * Inserta un nuevo registro en la tabla Persona
+     * @param persona 
+     */
+    public void InsertarPersona(Persona persona)
+    {
+     SessionFactory factory=HibernateUtil.getSessionFactory();
+     Session con=factory.openSession();
+     Transaction tx=con.beginTransaction();
+  
+     con.save(persona);
+     
+     tx.commit();
+     con.close();
+    }
+    
 }
